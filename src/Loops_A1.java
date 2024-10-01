7e2a5b09aa905e8f71e13600c471e55dbef1ddd3
Crxwny <damian.thoma@student.htldornbirn.at>
2024-10-01 16:36:48 +0200

    Loops_A1_finished

---
+++ src/Loops_A1.java
@@ -0,0 +1,13 @@
+public class Loops_A1 {
+    public static void main(String[] args) {
+
+        int summe = 0;
+        for (int i = 0; i <= 100 ; i++) {
+
+           summe+=i;
+
+        }
+
+        System.out.println(summe);
+    }
+}
