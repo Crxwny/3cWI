962bc3892bfcf6dcbd28039f7cd87ec4e5e1eaae
Crxwny <damian.thoma@student.htldornbirn.at>
2024-10-01 16:43:05 +0200

    Loops_A2_finished

---
+++ src/Loops_A2.java
@@ -0,0 +1,14 @@
+public class Loops_A2 {
+//Zähle die geraden Ziffern zwischen 1 und 1000 zusammen - Tip: Starte den Loop bei 2 und erhöhe den Zählindex jeweils um 2.
+    public static void main(String[] args) {
+        int summe = 0;
+
+        for (int i = 2; i <= 1000 ; i= i+2) {
+
+            summe += i;
+
+        }
+
+        System.out.println(summe);
+    }
+}
