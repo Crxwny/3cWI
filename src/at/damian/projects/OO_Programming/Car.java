06f605e6731c2e2b7964028d80d8f325645328f9
Damian <damian.thoma@student.htldornbirn.at>
2024-11-27 09:32:41 +0100

    OO_progression

--- src/at/damian/projects/OO_Programming/Car.java
+++ src/at/damian/projects/OO_Programming/Car.java
@@ -4,22 +4,89 @@ public class Car {
     // Instanz / Gedächtnisvariablen
 
     // don't do that later
-    public int fuelTank;
-    public int fuelConsumption;
-    public int fuelAmount;
-    public String brand;
-    public String serialNumber;
+    private int fuelTank;
+    private int fuelConsumption;
+    private int fuelAmount;
+    private String brand;
+    private String serialNumber;
     private String color;
+
+    public Car () {};
+
+    public Car(int fuelTank, int fuelConsumption, int fuelAmount, String brand, String serialNumber, String color) {
+        this.fuelTank = fuelTank;
+        this.fuelConsumption = fuelConsumption;
+        this.fuelAmount = fuelAmount;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getFuelTank() {
+        return fuelTank;
+    }
+
+    public void setFuelTank(int fuelTank) {
+        this.fuelTank = fuelTank;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public void setFuelAmount(int fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     // Methoden
     public void drive() {
-        this.fuelAmount = this.fuelAmount - fuelConsumption;
-        System.out.println("I am driving");
+        if (this.fuelAmount - this.fuelConsumption >= 0) {
+            this.fuelAmount = this.fuelAmount - fuelConsumption;
+            System.out.println("I am driving");
+        } else {
+            System.out.println("No fuel left to drive");
+            return;
+
+        }
     }
 
+
     public void doBreak() {
-        String brake = "Ich bremse";
+        String brake = "Hitting the brake";
         System.out.println(brake);
+
+
     }
 
     public void turboBoost() {
@@ -36,7 +103,8 @@ public class Car {
         }
     }
 
-    public void getRemainingRange() {
-        int remainingRange = this.fuelAmount - this.fuelConsumption;
+    public void getRemainingRangeFuel() {
+        int remainingRangeFuel = this.fuelAmount - this.fuelConsumption;
+        System.out.println("Remaining Fuel: "+remainingRangeFuel);
     }
 }
--- src/at/damian/projects/OO_Programming/Main.java
+++ src/at/damian/projects/OO_Programming/Main.java
@@ -4,28 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
 
         Car c1 = new Car();
-        c1.brand = "Audi";
-        c1.fuelConsumption = 7;
-        c1.serialNumber = "A1234";
-        c1.fuelAmount = 70;
-        c1.fuelTank = 70;
+        c1.setBrand("Audi");
+        c1.setFuelConsumption(7);
+        c1.setSerialNumber("A1234");
+        c1.setFuelAmount(70);
+        c1.setFuelTank(70);
+        c1.setColor("green");
 
 
-        Car c2 = new Car();
-        c2.brand = "Mercedes";
-        c2.fuelConsumption = 6;
-        c2.serialNumber = "M1234";
-        c2.fuelAmount = 20;
-        c2.fuelTank = 20;
+        Car c2 = new Car(20, 6, 20, "Mercedes", "M1234", "red");
 
 
-        System.out.println(c2.fuelAmount);
+
+
+        System.out.println(c2.getFuelAmount());
         c2.drive();
-        System.out.println(c2.fuelAmount);
+        System.out.println(c2.getFuelAmount());
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.print("Amount of Honks:");
+        c2.honk(scanner.nextInt());
+
+
+
+        c2.turboBoost();
+        c2.doBreak();
+        c2.drive();
+        c2.getRemainingRangeFuel();
 
 
     }
